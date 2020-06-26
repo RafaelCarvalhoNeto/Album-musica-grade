504e69e83f749427987759a7900a162bb1b29e06
Rafael Carvalho <rafaelcarvalhoneto@gmail.com>
2020-06-26 17:03:28 -0300

    testando-rolagem-com-js

---
+++ rolagem.js
@@ -0,0 +1,18 @@
+// var link = $(".link")
+// var at = $(".active")
+// var targetOffset = 0
+
+// link.on('click', function(e){
+//     e.preventDefault();
+//     var id = $(this).attr('href')
+//     targetOffset += Number($(id).offset().top)
+    
+//     link.removeClass("active")
+//     $(this).addClass("active")
+
+//     $(".content").animate({
+//         scrollTop: targetOffset
+//     }, 500)
+    
+//     console.log(targetOffset)
+// })
